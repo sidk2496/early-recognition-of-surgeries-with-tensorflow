80cf862241d45d582a094ab24c2da23192d90510
sidk2496 <sid2496@gmail.com>
2018-12-22 14:08:46 -0500

    Update lstm_build.py

--- model_build/lstm_build.py
+++ model_build/lstm_build.py
@@ -53,78 +53,6 @@ class LSTM(object):
                 accuracy = tf.reduce_mean(tf.cast(tf.equal(x=y_pred_cls, y=self.true_labels[:, -1]), tf.float32),
                                           axis=0)
 
-            elif loss_type == 'linear_weighted_avg':
-
-                y_true = tf.one_hot(indices=self.true_labels, depth=self._num_classes)
-                fn_loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=y_true))
-
-                fp_losses = -tf.reduce_sum((1 - y_true) * tf.log(1 - tf.nn.softmax(logits)), axis=2)
-                # ASSUMING BATCH_SIZE of 1
-                num_timesteps = tf.cast(tf.shape(logits)[1], tf.float32)
-                timestep_weights = tf.range(1, num_timesteps + 1)
-                fp_loss = tf.reduce_mean((timestep_weights * fp_losses
-                                          / num_timesteps))
-                loss = fp_loss + fn_loss
-
-                y_pred_cls = tf.argmax(logits[:, -1, :], axis=1)
-                accuracy = tf.reduce_mean(tf.cast(tf.equal(x=y_pred_cls, y=self.true_labels[:, -1]), tf.float32),
-                                          axis=0)
-
-            elif loss_type == 'linear_weighted_avg_2':
-
-                y_true = tf.one_hot(indices=self.true_labels, depth=self._num_classes)
-
-                # ASSUMING BATCH_SIZE of 1
-                num_timesteps = tf.cast(tf.shape(logits)[1], tf.float32)
-                timestep_weights = tf.range(1, num_timesteps + 1)
-                fn_loss = tf.reduce_mean(timestep_weights *
-                                         tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=y_true)
-                                         / num_timesteps)
-
-                fp_losses = -tf.reduce_sum((1 - y_true) * tf.log(1 - tf.nn.softmax(logits)), axis=2)
-                fp_loss = tf.reduce_mean(fp_losses)
-                loss = fp_loss + fn_loss
-
-                y_pred_cls = tf.argmax(logits[:, -1, :], axis=1)
-                accuracy = tf.reduce_mean(tf.cast(tf.equal(x=y_pred_cls, y=self.true_labels[:, -1]), tf.float32),
-                                          axis=0)
-
-            elif loss_type == 'exp_weighted_avg':
-
-                y_true = tf.one_hot(indices=self.true_labels, depth=self._num_classes)
-                fn_loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=y_true))
-
-                fp_losses = -tf.reduce_sum((1 - y_true) * tf.log(1 - tf.nn.softmax(logits)), axis=2)
-                # ASSUMING BATCH_SIZE of 1
-                num_timesteps = tf.cast(tf.shape(logits)[1], tf.float32)
-                timestep_weights = tf.exp(tf.range(1, num_timesteps + 1) - num_timesteps)
-                fp_loss = tf.reduce_mean((timestep_weights * fp_losses
-                                          / num_timesteps))
-                loss = fp_loss + fn_loss
-
-                y_pred_cls = tf.argmax(logits[:, -1, :], axis=1)
-                accuracy = tf.reduce_mean(tf.cast(tf.equal(x=y_pred_cls, y=self.true_labels[:, -1]), tf.float32),
-                                          axis=0)
-
-            elif loss_type == 'exp_weighted_avg_2':
-
-                y_true = tf.one_hot(indices=self.true_labels, depth=self._num_classes)
-
-                # ASSUMING BATCH_SIZE of 1
-                num_timesteps = tf.cast(tf.shape(logits)[1], tf.float32)
-                timestep_weights = tf.exp(tf.range(1, num_timesteps + 1) - num_timesteps)
-                fn_loss = tf.reduce_mean(timestep_weights *
-                                          tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=y_true)
-                                         / num_timesteps)
-
-                fp_losses = -tf.reduce_sum((1 - y_true) * tf.log(1 - tf.nn.softmax(logits)), axis=2)
-                fp_loss = tf.reduce_mean(fp_losses)
-                loss = fp_loss + fn_loss
-
-                y_pred_cls = tf.argmax(logits[:, -1, :], axis=1)
-                accuracy = tf.reduce_mean(tf.cast(tf.equal(x=y_pred_cls, y=self.true_labels[:, -1]), tf.float32),
-                                          axis=0)
-
             if add_l2_reg:
                 loss += weight_decay * tf.add_n([tf.nn.l2_loss(var) for var in tf.trainable_variables()])
 
@@ -142,41 +70,8 @@ class LSTM(object):
         # NOTE:- Batch size has to be 1.
         confidence = tf.reshape(tf.nn.softmax(logits), [-1, self._num_classes])
 
-        def _max(accum_val, conf):
-            curr_max = tf.reduce_max([accum_val, conf], axis=0)
-            return curr_max
-
-        def _avg(accum_val, conf):
-            curr_avg = tf.divide((accum_val[0] * accum_val[1]) + conf, accum_val[0] + 1)
-            return (accum_val[0] + 1, curr_avg)
-
-        def _weighted_avg(accum_val, conf):
-            curr_wt_avg = tf.divide(accum_val[1] * accum_val[0], accum_val[0] + 1) + conf
-            return (accum_val[0] + 1, curr_wt_avg)
-
-        def _moving_avg(accum_val, conf):
-            curr_mov_avg = 0.9 * accum_val + 0.1 * conf
-            return curr_mov_avg
-
         if pred_type == 'last':
             conf_agg = confidence
-
-        elif pred_type == 'max':
-            conf_agg = tf.scan(_max, confidence, initializer=-tf.ones(shape=[self._num_classes]))
-
-        elif pred_type == 'avg':
-            _, conf_agg = tf.scan(_avg, confidence, initializer=(tf.constant(0.0),
-                                                                 tf.zeros(shape=[self._num_classes])))
-
-        elif pred_type == 'weighted_avg':
-            _, conf_agg = tf.scan(_weighted_avg, confidence, initializer=(tf.constant(0.0),
-                                                                          tf.zeros(shape=[self._num_classes])))
-
-        elif pred_type == 'moving_avg':
-            conf_agg = tf.scan(_moving_avg, confidence, initializer=tf.zeros(shape=[self._num_classes]))
-
-        else:
-            raise ValueError('\'pred_type\' can be one of \'last\',  \'last\', \'avg\', \'max\' and \'weighted_avg\'')
 
         y_pred_cls = tf.expand_dims(tf.argmax(conf_agg, axis=1), 0)
         accuracy = tf.cast(tf.equal(x=y_pred_cls, y=self.true_labels), tf.float32)
